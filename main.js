61e39912b663c2daf197e1924ece1c38e8b56067
Jenny <156132099+Jennyznz@users.noreply.github.com>
2025-09-06 17:07:42 -0400

    Skeleton. And basic math.

---
+++ main.js
@@ -0,0 +1,15 @@
+function add(x, y) {
+    return (x + y);
+}
+
+function subtract(x, y) {
+    return (x - y);
+}
+
+function multiply(x, y) {
+    return (x * y);
+}
+
+function divide(x, y){
+    return (x / y); // all JS arithmetic is done in floating point
+}
